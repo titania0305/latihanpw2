8567d6089eb1d24d1ae9184bff68aa5ae228faef
titania0305 <titajambi132@gmail.com>
2024-12-18 10:41:58 +0700

    Add files via upload

---
+++ 1a.php
@@ -0,0 +1,8 @@
+<?php
+
+$a = "topi";
+$nama = "bundar";
+
+//isi variable $a
+
+echo $a . " saya " . $nama . "," . $nama . " " . $a . " saya ";
---
+++ 1b.php
@@ -0,0 +1,18 @@
+<?php
+
+$angka = 10;
+echo "aku adalah angka $angka. </br>" ;
+
+$angka *= 8;
+echo "jika aku dikali 8, jumlahku sekarang $angka </br>";
+
+$angka /= 4;
+echo "jika aku dibagi 4, jumlahku sekarang $angka </br>";
+
+$angka -= 6;
+echo "jika aku dikurang 6, jumlahku sekarang $angka </br>";
+
+$angka += 2;
+echo "jika aku ditambah 2, jumlahku sekarang $angka </br";
+
+?>
